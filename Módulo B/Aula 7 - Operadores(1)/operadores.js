35b0449bf57a094404ae9765048d8e407972b8db
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2024-08-09 22:36:30 -0300

    Anotações da Aula 07

---
+++ Módulo B/Aula 7 - Operadores(1)/operadores.js
@@ -0,0 +1,10 @@
+// Operadores Aritméticos
+var a = 5
+var b = 2
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+console.log(a++) // Incremento
+console.log(b--) // Decremento
