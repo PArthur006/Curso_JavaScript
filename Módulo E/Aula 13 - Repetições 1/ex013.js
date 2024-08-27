0f59d17bd95e801368700d2048ae02037c2063be
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2024-08-26 21:52:35 -0300

    Exercício 13 -> Laços de Repetição
    Laços While e Do While

---
+++ Módulo E/Aula 13 - Repetições 1/ex013.js
@@ -0,0 +1,13 @@
+//  WHILE
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//  DO WHILE
+var cont = 1
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+} while(cont <=6)
