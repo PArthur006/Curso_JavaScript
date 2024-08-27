1e61bd2dd5b1c4d64f30050e1ba6af6780543b74
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2024-08-27 17:24:32 -0300

    Correção do exercício
    Código do exercício no modelo do professor.

--- Módulo E/Exercício 4/script.js
+++ Módulo E/Exercício 4/script.js
@@ -1,23 +1,31 @@
-/*
-    inicio = 0
-    fim = 10
-    passo = 1
-    for (var c = inicio; c <= fim; c+=passo){
-        console.log(`Passo ${c}`)
-    }
-*/
 function verificar(){
-    let inicio = Number(document.getElementById("inicio").value)
-    let fim = Number(document.getElementById("fim").value)
-    let passo = Number(document.getElementById("passo").value)
-    if(passo <= 0 || inicio > fim){
-        alert("[ERRO] Informe os valores corretamente!")
+    let inicio = document.getElementById("inicio").value
+    let fim = document.getElementById("fim").value
+    let passo = document.getElementById("passo").value
+    let res = document.querySelector('div#res')
+
+    if(inicio.length == 0 || fim.length == 0 || passo.length == 0){
+        alert(`[ERRO] Faltam dados!`)
+        res.innerHTML = `Impossível contar!`
     } else{
-        var res = document.querySelector('div#res')
+        if(passo <= 0){
+            alert("Passo inválido! Considerando PASSO 1")
+            passo = 1
+        }
         res.innerHTML = `Contando...<br>`
-        for(var c = inicio; c <= fim; c += passo){
-            res.innerHTML += ` ${c} &#x1F449`
+
+        if(inicio < fim){
+            // Contagem Crescente:
+            for(let c = inicio; c <= fim; c += passo){
+                res.innerHTML += ` ${c} &#x1F449`
+            }
+        } else{ 
+            // Contagem Decrescente       
+            for(let c = inicio; c >= fim; c -= passo){
+                res.innerHTML += ` ${c} &#x1F449`
+            }
         }
         res.innerHTML += ` &#x1F3C1`
+        
     }
 }
